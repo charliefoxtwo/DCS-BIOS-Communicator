66660c2cf0abb576737267329ce4d2e0b55c2960
Charlie <30303272+charliefoxtwo@users.noreply.github.com>
2024-06-08 15:50:38 -0700

    Add parser tests

---
+++ DcsBiosCommunicatorTest/DataParserTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using DcsBios.Communicator.DataParsers;
+using NUnit.Framework;
+
+namespace DcsBiosCommunicatorTest;
+
+public class DataParserTests
+{
+    [Test]
+    public void Test2CharString()
+    {
+        var parser = new StringParser(0, 2, "ID");
+
+        const int data = 'a' | ('b' << 8);
+
+        parser.AddData(0, data);
+
+        Assert.That(parser.DataReady, Is.True);
+        Assert.That(parser.CurrentValue, Is.EqualTo("ab"));
+    }
+
+    [Test]
+    public void TestInt()
+    {
+        var parser = new IntegerParser(8, 3, "ID");
+
+        parser.AddData(0, 10);
+
+        Assert.That(parser.CurrentValue, Is.EqualTo((10 & 8) >> 3));
+    }
+}
