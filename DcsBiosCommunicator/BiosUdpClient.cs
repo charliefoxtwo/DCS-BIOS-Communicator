799cd8c6cea86d5795f9498ee0addcd5dd78c840
Charlie <30303272+charliefoxtwo@users.noreply.github.com>
2026-03-10 19:11:58 -0700

    Enable loggers to be null

--- DcsBiosCommunicator/BiosUdpClient.cs
+++ DcsBiosCommunicator/BiosUdpClient.cs
@@ -15,9 +15,9 @@ public class BiosUdpClient : IUdpReceiveClient, IBiosSendClient, IDisposable
     private readonly UdpClient _client;
     private readonly IPAddress _ipAddress;
     private readonly IPEndPoint _target;
-    private readonly ILogger<BiosUdpClient> _log;
+    private readonly ILogger<BiosUdpClient>? _log;
 
-    public BiosUdpClient(IPAddress ipAddress, int sendPort, int receivePort, in ILogger<BiosUdpClient> logger)
+    public BiosUdpClient(IPAddress ipAddress, int sendPort, int receivePort, in ILogger<BiosUdpClient>? logger)
     {
         _log = logger;
 
@@ -43,23 +43,23 @@ public class BiosUdpClient : IUdpReceiveClient, IBiosSendClient, IDisposable
     public async Task Send(string biosAddress, string data)
     {
         var message = $"{biosAddress} {data}{BlankBiosCommand}";
-        _log.LogDebug("Sending {{{BiosAddress} {Data}}} to DCS-BIOS", biosAddress, data);
+        _log?.LogDebug("Sending {{{BiosAddress} {Data}}} to DCS-BIOS", biosAddress, data);
         var byteData = Encoding.UTF8.GetBytes(message);
         await _client.SendAsync(byteData, byteData.Length, _target);
     }
 
     public void OpenConnection()
     {
-        _log.LogDebug("Opening connection to DCS-BIOS...");
+        _log?.LogDebug("Opening connection to DCS-BIOS...");
         _client.JoinMulticastGroup(_ipAddress);
-        _log.LogInformation("Connection to DCS-BIOS opened");
+        _log?.LogInformation("Connection to DCS-BIOS opened");
     }
 
     public void Close()
     {
-        _log.LogDebug("Closing connection to DCS-BIOS...");
+        _log?.LogDebug("Closing connection to DCS-BIOS...");
         _client.Close();
-        _log.LogInformation("Connection to DCS-BIOS closed");
+        _log?.LogInformation("Connection to DCS-BIOS closed");
     }
 
     public void Dispose()
